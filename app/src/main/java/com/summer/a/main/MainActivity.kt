a761ddd5b1b579d94fb0cc6cf61a2c75bdcfdcac
tangjie <tangjie@leadeon.cn>
2023-07-11 20:44:19 +0800

    Initial commit

---
+++ app/src/main/java/com/summer/a/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.summer.a.main
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity:FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
