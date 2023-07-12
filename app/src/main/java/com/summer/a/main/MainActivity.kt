8b9fd92c22875ae7b864d9cf61587c5b9689b984
tangjie <tangjie@leadeon.cn>
2023-07-12 23:20:01 +0800

    app module lib

--- app/src/main/java/com/summer/a/main/MainActivity.kt
+++ app/src/main/java/com/summer/a/main/MainActivity.kt
@@ -3,10 +3,12 @@ package com.summer.a.main
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.fragment.app.FragmentActivity
+import com.summer.a.R
 
 class MainActivity:FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
     }
 }
