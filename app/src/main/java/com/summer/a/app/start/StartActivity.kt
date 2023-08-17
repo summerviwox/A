7ec035f1acde9e4d477e92c01bb43e75626dbe00
tangjie <tangjie@leadeon.cn>
2023-08-17 16:50:14 +0800

    组件注解

--- app/src/main/java/com/summer/a/app/start/StartActivity.kt
+++ app/src/main/java/com/summer/a/app/start/StartActivity.kt
@@ -1,8 +1,8 @@
 package com.summer.a.app.start
 
 import android.os.Bundle
-import com.alibaba.android.arouter.launcher.ARouter
 import com.summer.a.lib.activity.BaseActivity
+import com.summer.a.lib.provider.ProviderManager
 import com.summer.a.lib.provider.RouterProvider
 
 class StartActivity: BaseActivity() {
@@ -14,9 +14,6 @@ class StartActivity: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(startView)
-        //ProviderManager.getProvider(RouterProvider::class.java)?.intent("/test/MainActivity")
-        //StartModel().jump(this,MainActivity::class.java)
-        var aa = AA("")
-
+        ProviderManager.getProvider(RouterProvider::class.java)?.intent("/test/MainActivity")
     }
 }
--- provider/src/main/java/com/summer/a/provider/ProviderProcessor.kt
+++ provider/src/main/java/com/summer/a/provider/ProviderProcessor.kt
@@ -2,20 +2,29 @@ package com.summer.a.provider
 
 import com.google.auto.service.AutoService
 import java.util.*
-import javax.annotation.processing.AbstractProcessor
-import javax.annotation.processing.ProcessingEnvironment
-import javax.annotation.processing.Processor
-import javax.annotation.processing.RoundEnvironment
+import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
 import javax.lang.model.element.TypeElement
+import javax.lang.model.type.MirroredTypeException
+import javax.lang.model.type.TypeMirror
+import javax.tools.FileObject
+import javax.tools.StandardLocation
+import kotlin.collections.ArrayList
+
 /**
  * 断点进不去就删掉build
  */
 @AutoService(Processor::class)
+
 class ProviderProcessor: AbstractProcessor() {
+
+    var filer:Filer? = null
 
     override fun init(processingEnv: ProcessingEnvironment?) {
         super.init(processingEnv)
+        filer = processingEnv?.filer
     }
 
     override fun getSupportedAnnotationTypes(): MutableSet<String> {
@@ -28,19 +37,58 @@ class ProviderProcessor: AbstractProcessor() {
     }
 
     override fun process(mutableSet: MutableSet<out TypeElement>?, roundEnvironment: RoundEnvironment?): Boolean {
-        println("1111111111111111111111")
-//        mutableSet?.forEach {
-//            set->
-//            var element = roundEnvironment?.getElementsAnnotatedWith(set)
-//
-//        }
-//        var set = roundEnvironment?.getElementsAnnotatedWith(ProviderAnno::class.java)
-//        set?.forEach {
-//            var TypeElement = it as TypeElement
-//            var c = Class.forName(TypeElement.qualifiedName.toString())
-//            var o = c.getDeclaredConstructor().newInstance()
-//
-//        }
+        var list:MutableList<MutableMap<String,String>> = mutableListOf()
+        mutableSet?.forEach {
+                set->
+            var element = roundEnvironment?.getElementsAnnotatedWith(set)
+            element?.forEach {
+                if(it.kind!=ElementKind.CLASS){
+                    return@forEach
+                }
+                var providerAnno:ProviderAnno = it.getAnnotation(ProviderAnno::class.java)
+                var value: String? = null
+                try {
+                    value = providerAnno.value.toString()
+                } catch (e: MirroredTypeException) {
+                    value = e.typeMirror.toString()
+                }
+                var typeElement = it as TypeElement
+                value?.apply {
+                    list.add(mutableMapOf(value to typeElement.qualifiedName.toString()))
+                }
+            }
+        }
+        generatorClass(list)
         return true
+    }
+
+    fun generatorClass(list:MutableList<MutableMap<String,String>>){
+
+        var file: FileObject? =null
+        try {
+            file = filer?.createResource(StandardLocation.SOURCE_OUTPUT,"","com.summer.a.provider.ProviderManager.kt")
+        } catch (e: Exception) {
+            e
+        }
+        var writer = file?.openWriter()
+        writer?.apply {
+            writer.write("package com.summer.a.lib.provider\n")
+            writer.write("class ProviderManager {\n")
+            writer.write("companion object{\n")
+            writer.write("private var map:MutableMap<Class<*>,Any> = mutableMapOf()\n")
+            writer.write("init {\n")
+            list.forEach {
+                writer.write("map.put(${it.keys.first()}::class.java,${it.values.first()}())\n")
+            }
+            writer.write("}\n")
+            writer.write("fun <T> getProvider(t:Class<T>):T?{\n")
+            writer.write("return map[t] as? T\n")
+            writer.write("}\n")
+            writer.write("}\n")
+            writer.write("}\n")
+            writer.flush()
+            writer.close()
+        }
+
     }
 }
