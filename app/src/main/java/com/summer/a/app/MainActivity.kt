5af5fb233ffcefeea0c9214ffef10d7e60609bc7
tangjie <tangjie@leadeon.cn>
2024-08-28 20:11:50 +0800

    PackageManagerHook

--- app/src/main/java/com/summer/a/app/MainActivity.kt
+++ app/src/main/java/com/summer/a/app/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.summer.a.app
 
+import android.content.pm.PackageInfo
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.blankj.utilcode.util.LogUtils
 import com.summer.a.app.databinding.MainActivityBinding
+import com.summer.a.app.hook.PackageManagerHook
 import com.summer.a.provider.RouteAnno
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -20,11 +23,19 @@ class MainActivity : FragmentActivity() {
         setContentView(mainActivityBinding.root)
         mainActivityBinding.c.setOnClickListener {
             // ProviderManager.getProvider(LoginProvider::class.java)?.login(this,"CustomWebActivity","code")
+            var packinfo =  packageManager.getPackageInfo("com.summer.a.app",0)
+            LogUtils.e(packinfo?.packageName)
 
         }
         //startActivity(Intent(this,LoginActivity::class.java))
 
+        PackageManagerHook.hook(this)
 
+
+
+    }
+
+    fun ab(){
         GlobalScope.launch(Dispatchers.Unconfined) {
             println("Thread: ${Thread.currentThread().name}")
             mainActivityBinding.c.setText("1212")
@@ -34,7 +45,6 @@ class MainActivity : FragmentActivity() {
             }
             println("Thread: ${Thread.currentThread().name}")
         }
-
     }
 
 
--- app/src/main/java/com/summer/a/app/start/StartActivity.kt
+++ app/src/main/java/com/summer/a/app/start/StartActivity.kt
@@ -3,6 +3,7 @@ package com.summer.a.app.start
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import com.summer.a.app.MainActivity
 import com.summer.a.module.album.AlbumActivity
 import com.summer.a.view.activity.BaseUIActivity
 
@@ -15,7 +16,7 @@ class StartActivity : BaseUIActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(startView)
-        //startActivity(Intent(this,MainActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
         //ProviderManager.getProvider(RouterProvider::class.java)?.intent(RouterPath._test_MainActivity)
 
     }
