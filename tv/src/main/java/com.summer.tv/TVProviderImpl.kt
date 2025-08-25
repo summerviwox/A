6a4fc2736346922feb6caf6a218c8391eee683f2
tangjie_dh <tangjie@arcvideo.com>
2025-08-26 00:30:28 +0800

    router1

--- tv/src/main/java/com.summer.tv/TVProviderImpl.kt
+++ tv/src/main/java/com.summer.tv/TVProviderImpl.kt
@@ -1,9 +1,10 @@
 package com.summer.tv
 
 import android.content.Context
+import com.alibaba.android.arouter.facade.annotation.Route
 import com.blankj.utilcode.util.LogUtils
 import com.summer.a.lib.provider.TVProvider
-
+@Route(path = "/tv/provider")
 class TVProviderImpl:TVProvider {
     override fun show() {
         LogUtils.e("${javaClass.name} show")
